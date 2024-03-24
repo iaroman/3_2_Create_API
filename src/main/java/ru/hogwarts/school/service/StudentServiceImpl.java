8cae30d5677c2370a5e282f8284b8d476d10a172
Роман <roma_yakushin@mail.ru>
2024-03-25 01:12:51 +0300

    4.6. Потоки v.2

--- src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/StudentServiceImpl.java
@@ -112,21 +112,23 @@ public class StudentServiceImpl implements StudentService{
                 .map(Student::getName)
                 .toList();
 
-        synchronized (namesStudent) {
-            System.out.println("get 0 = " + namesStudent.get(0));
-            System.out.println("get 1 = " + namesStudent.get(1));
+        System.out.println("get 0 = " + namesStudent.get(0));
+        System.out.println("get 1 = " + namesStudent.get(1));
 
-            new Thread(() -> {
-                logger.info("Start synchronized Thread One");
+        new Thread(() -> {
+            logger.info("Start synchronized Thread One");
+            synchronized (this) {
                 System.out.println("get 2 = " + namesStudent.get(2));
                 System.out.println("get 3 = " + namesStudent.get(3));
-            }).start();
+            }
+        }).start();
 
-            new Thread(() -> {
-                logger.info("Start synchronized Thread two");
+        new Thread(() -> {
+            logger.info("Start synchronized Thread two");
+            synchronized (this) {
                 System.out.println("get 4 = " + namesStudent.get(4));
                 System.out.println("get 5 = " + namesStudent.get(5));
-            }).start();
-        }
+            }
+        }).start();
     }
 }
